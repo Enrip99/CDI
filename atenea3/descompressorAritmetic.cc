f0f194771d2ebe97117fedb30f75d27df2cca2be
enrip99 <enric.1999@hotmail.com>
2025-03-03 22:02:47 +0100

    enrip learns linear interpolation

--- atenea3/descompressorAritmetic.cc
+++ atenea3/descompressorAritmetic.cc
@@ -19,7 +19,7 @@ vecStruct recalcularCotes(vecStruct vin, int min, int max, int sumaGlobal){
     vecStruct nou (vin.size() + 1);
     int sumatori = 0;
     for (int i = 0; i < vin.size(); ++i){
-        nou[i] = parStruct (vin[i].first, min + (sumatori * max) / sumaGlobal);
+        nou[i] = parStruct (vin[i].first, ((sumaGlobal - sumatori) * min + sumatori * max)/sumaGlobal);
         sumatori += vin[i].second;
     }
     nou[nou.size() - 1] = parStruct(NULL,max);
@@ -91,9 +91,9 @@ int main(){
     vecStruct vectorActual = recalcularCotes(decodificador, 0, cotaMax, total);
     int cotaMinAct = 0, cotaMaxAct = cotaMax;
 
-    for (int i = 0; i < vectorActual.size(); ++i){
+    /*for (int i = 0; i < vectorActual.size(); ++i){
         std::cout << vectorActual[i].first << " - " << vectorActual[i].second << std::endl;
-    }
+    }*/
 
     int fraccio = 0, i = 0;
     for (; i < bitsPerNum; ++i){
@@ -107,47 +107,52 @@ int main(){
 
 
     std::string missatge;
-    std::cout << "PRE: i: " << i << " -fraccio: " << fraccio << " - cotaminact: " << cotaMinAct << " - cotamaxact: " << cotaMaxAct << std::endl;
+    //std::cout << "PRE: i: " << i << " - CotaMinAct: " << cotaMinAct << " - fraccio: " << fraccio << " - CotaMaxAct: " << cotaMaxAct << std::endl;
 
     while (i < codi.size()){
     //    if (fraccio < 5000 && fraccio > -1)std::cout << fraccio << std::endl;
         if (cotaMaxAct < meitat){
             //E0
+            std::cout << "E0: i: " << i << " - CotaMinAct: " << cotaMinAct << " - fraccio: " << fraccio << " - CotaMaxAct: " << cotaMaxAct << std::endl;
             cotaMinAct = cotaMinAct * 2;
             cotaMaxAct = cotaMaxAct * 2;
             fraccio = fraccio * 2;
             if (codi[i] == '1') ++fraccio;
             ++i;
-            std::cout << "E0: i: " << i << " -fraccio: " << fraccio << " - cotaminact: " << cotaMinAct << " - cotamaxact: " << cotaMaxAct << std::endl;
         }
         else if (cotaMinAct > meitat){
             //E1
+            std::cout << "E1: i: " << i << " - CotaMinAct: " << cotaMinAct << " - fraccio: " << fraccio << " - CotaMaxAct: " << cotaMaxAct << std::endl;
             cotaMinAct = cotaMinAct * 2 - cotaMax;
             cotaMaxAct = cotaMaxAct * 2 - cotaMax;
             fraccio = fraccio * 2 - cotaMax;
             if (codi[i] == '1') ++fraccio;
             ++i;
-            std::cout << "E1: i: " << i << " - fraccio: " << fraccio << " - cotaminact: " << cotaMinAct << " - cotamaxact: " << cotaMaxAct << std::endl;
         }
         else if (cotaMinAct > quart && cotaMaxAct < tresquart){
             //E2
+            std::cout << "E2: i: " << i << " - CotaMinAct: " << cotaMinAct << " - fraccio: " << fraccio << " - CotaMaxAct: " << cotaMaxAct << std::endl;
             cotaMinAct = cotaMinAct * 2 - meitat;
             cotaMaxAct = cotaMaxAct * 2 - meitat;
             fraccio = fraccio * 2 - meitat;
             if (codi[i] == '1') ++fraccio;
             ++i;
-            std::cout << "E2: i: " << i << " - fraccio: " << fraccio << " - cotaminact: " << cotaMinAct << " - cotamaxact: " << cotaMaxAct << std::endl;
         }
         else{
             vectorActual = recalcularCotes(decodificador, cotaMinAct, cotaMaxAct, total);
             //....
+
+
             int j = 0;
-            for (; j < vectorActual.size() && fraccio > vectorActual[j+1].second; ++j);
-            //std::cout << j << " - " << vectorActual[j].first << std::endl;
+            for (; j < vectorActual.size() && fraccio >= vectorActual[j+1].second; ++j);
             missatge.push_back(vectorActual[j].first);
+            
+            std::cout << std::endl << "HIT: i: " << i << " - CotaMinAct: " << cotaMinAct << " - fraccio: " << fraccio << " - CotaMaxAct: " << cotaMaxAct << std::endl;
+            for (int i = 0; i < vectorActual.size(); ++i)   std::cout << vectorActual[i].first << " - " << vectorActual[i].second << std::endl;
+            std::cout << "lletra: " << vectorActual[j].first << std::endl << "missatge: " << missatge << std::endl;
+            
             cotaMinAct = vectorActual[j].second;
             cotaMaxAct = vectorActual[j+1].second;
-            std::cout << "HIT: i: " << i << " - fraccio: " << fraccio << " - cotaminact: " << cotaMinAct << " - cotamaxact: " << cotaMaxAct << std::endl;
         }
     }
     std::cout << missatge.substr(0,longitud) << std::endl;
