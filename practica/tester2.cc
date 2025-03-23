99bf221bd37c1bc4f7fd5b516668eafa956582e8
enrip99 <enric.1999@hotmail.com>
2025-03-23 19:43:00 +0100

    fitxers de proves

---
+++ practica/tester2.cc
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+#include <filesystem>
+
+int main(int argc, char ** argv){
+    std::filesystem::path p{argv[1]};
+
+    FILE * entrada;
+    entrada = fopen (argv[1], "r");
+    __uint8_t lectura[2048];
+    std::vector<long> count (256,0);
+    int max;
+    while (max = fread(lectura, sizeof(__uint8_t), 2048, entrada)){
+        for (int i = 0; i < max; ++i){
+            count[lectura[i]]++;
+        }
+    }
+
+    for (int i = 0; i < 256; ++i ){
+        std::cout << count[i] << std::endl;
+    }
+
+}
